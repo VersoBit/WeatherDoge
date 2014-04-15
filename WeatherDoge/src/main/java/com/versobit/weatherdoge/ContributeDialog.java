01e9120e29e19dbf99d75a7cdafce65a5ba9d338
Kevin Mark <kevin.mark@about.me>
2014-04-14 21:58:31 -0400

    Added GPL and explicit copyright headers
    An explicit copyright notice along with a GPL header has been added to
    source components of Weather Doge.

--- WeatherDoge/src/main/java/com/versobit/weatherdoge/ContributeDialog.java
+++ WeatherDoge/src/main/java/com/versobit/weatherdoge/ContributeDialog.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 VersoBit Media, LLC
+ *
+ * This file is part of Weather Doge.
+ *
+ * Weather Doge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Weather Doge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Weather Doge.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.versobit.weatherdoge;
 
 import android.annotation.SuppressLint;
