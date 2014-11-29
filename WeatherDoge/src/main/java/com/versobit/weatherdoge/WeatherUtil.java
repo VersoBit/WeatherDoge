50ab7ed861bd1421c10dc5fff754c24cc68e1a11
Kevin Mark <kevin@versobit.com>
2014-11-29 12:46:48 -0500

    Drastically shortened convertYahooCode switch

--- WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
+++ WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
@@ -212,57 +212,27 @@ final class WeatherUtil {
         String owmCode = "01";
         switch (Integer.parseInt(code)) {
             // Thunderstorms
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 17:
-            case 37:
-            case 38:
-            case 39:
-            case 45:
-            case 47:
+            case 0: case 1: case 2: case 3: case 4: case 17: case 37: case 38: case 39: case 45: case 47:
                 owmCode = "11";
                 break;
             // Snow
-            case 5:
-            case 7:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 18:
-            case 41:
-            case 42:
-            case 43:
-            case 46:
+            case 5: case 7: case 13: case 14: case 15: case 16: case 18: case 41: case 42: case 43: case 46:
                 owmCode = "13";
                 break;
             // Rain
-            case 6:
-            case 10:
-            case 11:
-            case 12:
-            case 35:
+            case 6: case 10: case 11: case 12: case 35:
                 owmCode = "10";
                 break;
             // Light Rain
-            case 8:
-            case 9:
-            case 40:
+            case 8: case 9: case 40:
                 owmCode = "09";
                 break;
             // Fog
-            case 19:
-            case 20:
-            case 21:
-            case 22:
+            case 19: case 20: case 21: case 22:
                 owmCode = "50";
                 break;
             // Cloudy
-            case 27:
-            case 28:
+            case 27: case 28:
                 owmCode = "04";
                 break;
             // (Other) Cloudy
@@ -270,20 +240,11 @@ final class WeatherUtil {
                 owmCode = "03";
                 break;
             // Partly Cloudy
-            case 23:
-            case 24:
-            case 25:
-            case 29:
-            case 30:
-            case 44:
+            case 23: case 24: case 25: case 29: case 30: case 44:
                 owmCode = "02";
                 break;
             // Clear
-            case 31:
-            case 32:
-            case 33:
-            case 34:
-            case 36:
+            case 31: case 32: case 33: case 34: case 36:
                 owmCode = "01";
                 break;
         }
