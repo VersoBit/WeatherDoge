560bf7520847ae5e1e69b66cda0a411ae5abdd74
Kevin Mark <kevin@versobit.com>
2015-05-23 04:33:26 -0400

    Remove bash build dependency. Add flavor to vname

--- WeatherDoge/versionCode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-head -n 1 $DIR/versionDef.txt
--- WeatherDoge/versionName.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-printf `sed -n '2p' < "$DIR/versionDef.txt"`-
-git --git-dir="$DIR/../.git" --work-tree="$DIR/.." rev-parse --short HEAD 2> /dev/null
