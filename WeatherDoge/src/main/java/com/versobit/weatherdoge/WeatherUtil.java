e4db9c1cfcc78234cb9ce803505f6d18dffd7545
Kevin Mark <kevin@versobit.com>
2016-08-01 18:26:54 -0700

    Constant locale for Yahoo date format

--- WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
+++ WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
@@ -48,8 +48,7 @@ final class WeatherUtil {
     private static final String TAG = WeatherUtil.class.getSimpleName();
 
     private static final Pattern YAHOO_TIME = Pattern.compile("([0-9]{1,2}):([0-9]{2}) (am|pm)");
-    @SuppressLint("SimpleDateFormat")
-    private static final SimpleDateFormat YAHOO_DATE_FORMAT = new SimpleDateFormat("EEE, d MMM yyyy h:m a zzz");
+    private static final SimpleDateFormat YAHOO_DATE_FORMAT = new SimpleDateFormat("EEE, d MMM yyyy h:m a zzz", Locale.US);
 
     private WeatherUtil() {
         //
