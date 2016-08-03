9d1f559d2e0ddd91398da29e5ed0954b7ff3aa53
Kevin Mark <kevin@versobit.com>
2016-08-02 21:58:15 -0700

    Accept "6:7 am" as a valid time
    The Yahoo! Weather API would sometimes return values like “6:7 am” for
    the sunset/sunrise time instead of “6:07 am” When this happens the
    daytime check would fail.

--- WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
+++ WeatherDoge/src/main/java/com/versobit/weatherdoge/WeatherUtil.java
@@ -50,7 +50,7 @@ final class WeatherUtil {
 
     private static final String TAG = WeatherUtil.class.getSimpleName();
 
-    private static final Pattern YAHOO_TIME = Pattern.compile("([0-9]{1,2}):([0-9]{2}) (am|pm)");
+    private static final Pattern YAHOO_TIME = Pattern.compile("([0-9]{1,2}):([0-9]{1,2}) (am|pm)");
     private static final SimpleDateFormat YAHOO_DATE_FORMAT = new SimpleDateFormat("EEE, d MMM yyyy h:m a zzz", Locale.US);
 
     private WeatherUtil() {
