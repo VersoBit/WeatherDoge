c5051b8f797e68f16c7ff23f5d2e8dbdc12fab07
Kevin Mark <kevin@versobit.com>
2019-02-07 04:31:50 -0500

    Prevent Other Shibes crash on API >= 24

--- WeatherDoge/src/main/java/com/versobit/weatherdoge/dialogs/OtherShibesDialog.java
+++ WeatherDoge/src/main/java/com/versobit/weatherdoge/dialogs/OtherShibesDialog.java
@@ -20,12 +20,16 @@
 package com.versobit.weatherdoge.dialogs;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.versobit.weatherdoge.BuildConfig;
 import com.versobit.weatherdoge.R;
@@ -41,6 +45,34 @@ public final class OtherShibesDialog extends DialogFragment {
         wv.getSettings().setDefaultTextEncodingName("utf-8");
         wv.getSettings().setJavaScriptEnabled(true);
         wv.addJavascriptInterface(new JsInterface(), "App");
+        // Much more complex than it needs to be but should work until we move away from this
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            @TargetApi(Build.VERSION_CODES.N)
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (!request.getMethod().equals("GET")) {
+                    // GET only
+                    return true;
+                }
+                String scheme = request.getUrl().getScheme();
+                if ("https".equals(scheme) || "http".equals(scheme)) {
+                    // Let request through
+                    return false;
+                }
+                String url = request.getUrl().toString();
+                // With just 3 URLs this naive way is probably the fastest way to do it
+                if ("file:///android_asset/othershibes.html".equals(url) ||
+                        "file:///android_asset/gpl.html".equals(url)) {
+                    wv.loadUrl(url);
+                }
+                if (BuildConfig.FLAVOR.equals(BuildConfig.FLAVOR_PLAY) &&
+                        "file:///android_asset/comicsans.html".equals(url)) {
+                    wv.loadUrl(url);
+                }
+                // Block WebView from loading request
+                return true;
+            }
+        });
         wv.loadUrl("file:///android_asset/othershibes.html");
         return new AlertDialog.Builder(getActivity(), getTheme())
                 .setView(wv)
