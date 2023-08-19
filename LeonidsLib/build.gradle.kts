dc6891734ff72e3ce5f7b26edcf0cf5c7533161a
Kevin Mark <kmark937@gmail.com>
2023-08-19 02:48:11 -0400

    Add LeonidsLib 1.3.2 directly to repo

---
+++ LeonidsLib/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.plattysoft.leonids.leonidslib"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 11
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {}
